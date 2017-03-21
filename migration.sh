69d9bbb683465f5d4c705b88b9b39833299dd0b8
daton89 <tonydangelo123@gmail.com>
2017-03-21 23:38:14 +0100

    added shell script

---
+++ migration.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: migration.sh
+# 
+#         USAGE: ./migration.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: daton (tony@jointloop.ovh), 
+#  ORGANIZATION: Jointloop
+#       CREATED: 21.03.2017 23:35:07
+#      REVISION:  1.0
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+

de633e931d0e83885fa5868aa1bde951391ff864
daton89 <tonydangelo123@gmail.com>
2017-03-26 22:18:09 +0200

    updated migration

--- migration.sh
+++ migration.sh
@@ -19,4 +19,5 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-
+rsync -auvz --progress jl:/var/www/vhosts /Volumes/OSX\ 1/backup_jointloop_server
+rsync -auvz --progress jl:/root/backup-servers /Volumes/OSX\ 1/backup_jointloop_server

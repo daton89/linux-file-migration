9a793db22bd6b6deff21f5bb3b653bd60aa7cd62
daton89 <tonydangelo123@gmail.com>
2017-03-26 22:18:23 +0200

    added js version with yargs

---
+++ migration.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var argv = require('yargs')
+    .usage('Usage: $0 -w [num] -h [num]')
+    .demandOption(['w', 'h'])
+    .argv;
+
+console.log("The area is:", argv.w * argv.h);

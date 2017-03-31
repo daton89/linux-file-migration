34003f40a418a78974d375ee3d5baab74b49a7fb
daton <tonydangelo123@gmail.com>
2017-03-31 17:40:18 +0200

    added prompt and shelljs to migration tool

--- migration.js
+++ migration.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+var prompt = require('prompt');
+var shell = require('shelljs');
+
 var argv = require('yargs')
     .usage('Usage: $0 -w [num] -h [num]')
     .demandOption(['w', 'h'])
